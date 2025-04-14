811d738928b0be56ca00f5a80f215e420ab3b6c1
Yoav Sternberg <yoav.sternberg@gmail.com>
2025-04-14 10:23:14 +0300

    :art: Optimize matching for selector

--- src/objchelper/plugins/objc_selector/objc_selector.py
+++ src/objchelper/plugins/objc_selector/objc_selector.py
@@ -1,18 +1,41 @@
 __all__ = ["objc_selector_hexrays_hooks_t"]
 
+import re
+
 import ida_hexrays
 from ida_hexrays import Hexrays_Hooks, carg_t, carglist_t, cexpr_t, cfunc_t, citem_t
 from ida_kernwin import simpleline_t
-from ida_lines import COLOR_ON, COLSTR, SCOLOR_ADDR, SCOLOR_LOCNAME, SCOLOR_SYMBOL, tag_remove
+from ida_lines import COLOR_OFF, COLOR_ON, COLSTR, SCOLOR_ADDR, SCOLOR_LOCNAME, SCOLOR_SYMBOL, tag_remove
 from ida_pro import strvec_t
 
 from objchelper.idahelper.ast import cexpr
 
 SELECTOR_MARKER = "!@#$sel$#@!"
 COMMA_COLORED = COLSTR(",", SCOLOR_SYMBOL)
-COMMA_COLORED_SPACE = COMMA_COLORED + " "
 INSIGNIFICANT_LENGTH_FOR_LINE = 5
 MAX_LINE_SIZE = 120
+
+SEL_TOKEN_REGEX = re.compile(
+    "(?P<prefix>"
+    + re.escape(COMMA_COLORED + " ")
+    + r")?"
+    + re.escape(COLOR_ON + SCOLOR_ADDR)
+    + r"(?P<index>[0-9A-Fa-f]{16})"
+    + re.escape(COLOR_ON + SCOLOR_ADDR)
+    + r"(?P=index)"
+    + re.escape(COLSTR('"', SCOLOR_SYMBOL))
+    + re.escape(COLOR_ON + SCOLOR_LOCNAME)
+    + r"(?P<selector>[A-Za-z0-9_:]+)"
+    + '"'
+    + re.escape(COLOR_OFF + SCOLOR_LOCNAME)
+    + "(?P<postfix>"
+    + re.escape(COMMA_COLORED)
+    + r" ?)?"
+)
+"""
+a regex for a possible selector in IDA's pseudocode, with support for the prefix ", " or the postfix ","
+Its groups are: prefix, index, selector and postfix.
+"""
 
 
 def is_objc_method(name: str) -> bool:
@@ -21,15 +44,15 @@ def is_objc_method(name: str) -> bool:
 
 
 class objc_selector_hexrays_hooks_t(Hexrays_Hooks):
-    def func_printed(self, cfunc: cfunc_t) -> "int":
+    def func_printed(self, cfunc: cfunc_t) -> int:
         selectors_to_remove: dict[int, str] = {}  # obj_id -> selector
-        index_to_sel: list[(int, str)] = []  # index, selector
+        index_to_sel: dict[int, str] = {}  # index, selector
 
         for i, call_item in enumerate(cfunc.treeitems):
             call_item: citem_t
             # Get the index of the selector AST element
             if call_item.obj_id in selectors_to_remove:
-                index_to_sel.append((i, selectors_to_remove.pop(call_item.obj_id)))
+                index_to_sel[i] = selectors_to_remove.pop(call_item.obj_id)
             elif call_item.op == ida_hexrays.cot_call:
                 call_expr: cexpr_t = call_item.cexpr
 
@@ -57,31 +80,37 @@ class objc_selector_hexrays_hooks_t(Hexrays_Hooks):
         return 0
 
 
-def modify_text(cfunc: cfunc_t, index_to_sel: list[(int, str)]):
+def modify_text(cfunc: cfunc_t, index_to_sel: dict[int, str]):
     # Early return if no tokens to replace
     if not index_to_sel:
         return
-
-    selector_tokens = [sel_to_token(i, sel) for i, sel in index_to_sel]
 
     ps: strvec_t = cfunc.get_pseudocode()
     lines_marked_for_removal: list[simpleline_t] = []
     for i, line in enumerate(ps):
         line: simpleline_t
         should_merge = False
-        for j in range(len(selector_tokens) - 1, -1, -1):
-            line_text: str = line.line
-            # Check if selector token is in the line, try to expand with commas and spaces
-            if (selector_token := maximal_token_in_line(selector_tokens[j], line_text)) is None:
-                continue
 
-            # We expect to see each selector once, so we can remove the current token
-            del selector_tokens[j]
+        # Reverse the results so indices will not change
+        for result in reversed(list(re.finditer(SEL_TOKEN_REGEX, line.line))):
+            result: re.Match
+            index = int(result.group("index"), 16)
+            if index in index_to_sel:
+                # We found a selector token, remove it from the list
+                sel = index_to_sel.pop(index)
+                if sel != result.group("selector"):
+                    print("[Error]: selector mismatch. Expected:", sel, "Actual:", result.group("selector"))
+                    continue
 
-            # Update line's "should merge with previous" and text
-            line_text_with_marker = line_text.replace(selector_token, SELECTOR_MARKER)
-            should_merge = should_merge or should_merge_line(line_text_with_marker, ps[i - 1].line)
-            line.line = line_text_with_marker.replace(SELECTOR_MARKER, "")
+                # If match contains both a prefix and a postfix, remove only the prefix
+                left, right = result.span()
+                if result.group("prefix") and result.group("postfix"):
+                    right = result.start("postfix")
+
+                # Remove the selector, check if we need to merge lines
+                before_selector, after_selector = line.line[: result.span()[0]], line.line[right:]
+                should_merge = should_merge or should_merge_line(before_selector, after_selector, ps[i - 1].line)
+                line.line = before_selector + after_selector
 
         if should_merge:
             lines_marked_for_removal.append(line)
@@ -92,43 +121,17 @@ def modify_text(cfunc: cfunc_t, index_to_sel: list[(int, str)]):
         ps.erase(line_to_remove)
 
 
-def maximal_token_in_line(selector_token: str, line: str) -> str | None:
-    """
-    Given a selector token and a line, Check if it is in the line.
-    If it is, try to extend it with spaces and unnecessary commas.
-    Return the token with the extended spaces and commas.
-    If it is not in the line, return None.
-    """
-    try:
-        # Check if the token is in the line
-        selector_index = line.index(selector_token)
-        # Check if the token is preceded by a comma and a space, so we can remove the comma
-        if line.startswith(COMMA_COLORED_SPACE, selector_index - len(COMMA_COLORED_SPACE)):
-            selector_token = COMMA_COLORED_SPACE + selector_token
-        # If the token is succeeded by a comma, we can remove the comma
-        elif line.startswith(COMMA_COLORED, selector_index + len(selector_token)):
-            selector_token += COMMA_COLORED
-            # If there is an additional space, remove it as well
-            if line.startswith(" ", selector_index + len(selector_token) + len(COMMA_COLORED)):
-                selector_token += " "
-    except ValueError:
-        return None
-    else:
-        return selector_token
-
-
-def should_merge_line(line: str, prev_line: str) -> bool:
+def should_merge_line(before_selector: str, after_selector: str, prev_line: str) -> bool:
     """
     Given a `line` with a selector marker and the previous line, should we merge the two lines.
     """
-    before, after = line.split(SELECTOR_MARKER)
-    before_without_tags = tag_remove(before)
+    before_without_tags = tag_remove(before_selector)
 
     # We only remove lines that starts with the selector
     if before_without_tags and not before_without_tags.isspace():
         return False
 
-    after_without_tags = tag_remove(after)
+    after_without_tags = tag_remove(after_selector)
     # If the line is short, allow merging
     if len(after_without_tags) < INSIGNIFICANT_LENGTH_FOR_LINE:
         return True
@@ -136,13 +139,6 @@ def should_merge_line(line: str, prev_line: str) -> bool:
     # Merge if it will not lead to a long line
     prev_line_without_tags = tag_remove(prev_line)
     return len(after_without_tags) + len(prev_line_without_tags) < MAX_LINE_SIZE
-
-
-def sel_to_token(index: int, sel: str) -> str:
-    """Return the tokens that represent the selector in IDA's pseudocode"""
-    color_obj_id = COLOR_ON + SCOLOR_ADDR + to_hex(index, length=16)
-    existing_token = color_obj_id + color_obj_id + COLSTR('"', SCOLOR_SYMBOL) + COLSTR(sel + '"', SCOLOR_LOCNAME)
-    return existing_token
 
 
 def to_hex(n: int, *, length: int) -> str:
