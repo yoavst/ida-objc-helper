acf5c45db8feddab426d703133618a43dcb6e224
Yoav Sternberg <yoav.sternberg@gmail.com>
2025-04-14 10:53:14 +0300

    :sparkles: remove class from static calls

--- src/objchelper/plugins/objc_selector/objc_selector.py
+++ src/objchelper/plugins/objc_selector/objc_selector.py
@@ -5,7 +5,17 @@ import re
 import ida_hexrays
 from ida_hexrays import Hexrays_Hooks, carg_t, carglist_t, cexpr_t, cfunc_t, citem_t
 from ida_kernwin import simpleline_t
-from ida_lines import COLOR_OFF, COLOR_ON, COLSTR, SCOLOR_ADDR, SCOLOR_LOCNAME, SCOLOR_SYMBOL, tag_remove
+from ida_lines import (
+    COLOR_OFF,
+    COLOR_ON,
+    COLSTR,
+    SCOLOR_ADDR,
+    SCOLOR_DEMNAME,
+    SCOLOR_IMPNAME,
+    SCOLOR_LOCNAME,
+    SCOLOR_SYMBOL,
+    tag_remove,
+)
 from ida_pro import strvec_t
 
 from objchelper.idahelper.ast import cexpr
@@ -37,22 +47,63 @@ a regex for a possible selector in IDA's pseudocode, with support for the prefix
 Its groups are: prefix, index, selector and postfix.
 """
 
+# noinspection RegExpDuplicateCharacterInClass
+CLASS_TOKEN_REGEX = re.compile(
+    re.escape(COLOR_ON + SCOLOR_ADDR)
+    + r"(?P<index>[0-9A-Fa-f]{16})"
+    + re.escape(COLSTR("&", SCOLOR_SYMBOL))
+    + re.escape(COLOR_ON + SCOLOR_ADDR)
+    + r"(?P<index2>[0-9A-Fa-f]{16})"
+    + re.escape(COLOR_ON)
+    + "["
+    + re.escape(SCOLOR_IMPNAME)
+    + "|"
+    + re.escape(SCOLOR_DEMNAME)
+    + "]"
+    + "OBJC_CLASS___"
+    + "(?P<class>[A-Za-z0-9_]+)"
+    + re.escape(COLOR_OFF)
+    + "["
+    + re.escape(SCOLOR_IMPNAME)
+    + "|"
+    + re.escape(SCOLOR_DEMNAME)
+    + "]"
+    + "(?P<postfix>"
+    + re.escape(COMMA_COLORED)
+    + r" ?)?"
+)
+"""
+a regex for possible obj-c class in IDA's pseudocode, with support for the postfix ","
+Its groups are: index, index2, class and postfix.
+"""
+
 
 def is_objc_method(name: str) -> bool:
     """Does the name look like an Obj-C method?"""
     return len(name) > 3 and name[0] in ["-", "+"] and name[1] == "[" and name[-1] == "]"
 
 
+def is_objc_static_method(name: str) -> bool:
+    """Given obj-C method name, check if it is a static method."""
+    return name[0] == "+"
+
+
 class objc_selector_hexrays_hooks_t(Hexrays_Hooks):
-    def func_printed(self, cfunc: cfunc_t) -> int:
+    def func_printed(self, cfunc: cfunc_t) -> int:  # noqa: C901
         selectors_to_remove: dict[int, str] = {}  # obj_id -> selector
+        classes_to_remove: set[int] = set()  # obj_id
         index_to_sel: dict[int, str] = {}  # index, selector
+        class_indices_to_remove: set[int] = set()  # index
 
         for i, call_item in enumerate(cfunc.treeitems):
             call_item: citem_t
-            # Get the index of the selector AST element
+            # Get the index of the selector/class AST element
             if call_item.obj_id in selectors_to_remove:
                 index_to_sel[i] = selectors_to_remove.pop(call_item.obj_id)
+            elif call_item.obj_id in classes_to_remove:
+                class_indices_to_remove.add(i)
+                classes_to_remove.remove(call_item.obj_id)
+
             elif call_item.op == ida_hexrays.cot_call:
                 call_expr: cexpr_t = call_item.cexpr
 
@@ -70,17 +121,26 @@ class objc_selector_hexrays_hooks_t(Hexrays_Hooks):
                 if sel_arg.op != ida_hexrays.cot_str:
                     print("[Error]: Obj-C method call with non-string selector:", call_expr.dstr())
                     continue
-
                 selectors_to_remove[sel_arg.obj_id] = sel_arg.string
 
-        if selectors_to_remove:
+                # 3. Check if the function is a class method
+                if is_objc_static_method(call_func_name):
+                    # 4. Check if the class name is a ref to obj
+                    class_arg: carg_t = arglist[0]
+                    if class_arg.op != ida_hexrays.cot_ref or class_arg.x.op != ida_hexrays.cot_obj:
+                        print("[Error]: Obj-C class method with unsupported class", call_expr.dstr())
+                        continue
+                    # 5. Collect the class name
+                    classes_to_remove.add(class_arg.obj_id)
+
+        if selectors_to_remove or classes_to_remove:
             print("[Error]: unmatched Obj-C selectors in the function: ", hex(cfunc.entry_ea))
         elif index_to_sel:
-            modify_text(cfunc, index_to_sel)
+            modify_text(cfunc, index_to_sel, class_indices_to_remove)
         return 0
 
 
-def modify_text(cfunc: cfunc_t, index_to_sel: dict[int, str]):
+def modify_text(cfunc: cfunc_t, index_to_sel: dict[int, str], class_indices_to_remove: set[int]):
     # Early return if no tokens to replace
     if not index_to_sel:
         return
@@ -89,28 +149,9 @@ def modify_text(cfunc: cfunc_t, index_to_sel: dict[int, str]):
     lines_marked_for_removal: list[simpleline_t] = []
     for i, line in enumerate(ps):
         line: simpleline_t
-        should_merge = False
-
-        # Reverse the results so indices will not change
-        for result in reversed(list(re.finditer(SEL_TOKEN_REGEX, line.line))):
-            result: re.Match
-            index = int(result.group("index"), 16)
-            if index in index_to_sel:
-                # We found a selector token, remove it from the list
-                sel = index_to_sel.pop(index)
-                if sel != result.group("selector"):
-                    print("[Error]: selector mismatch. Expected:", sel, "Actual:", result.group("selector"))
-                    continue
-
-                # If match contains both a prefix and a postfix, remove only the prefix
-                left, right = result.span()
-                if result.group("prefix") and result.group("postfix"):
-                    right = result.start("postfix")
-
-                # Remove the selector, check if we need to merge lines
-                before_selector, after_selector = line.line[: result.span()[0]], line.line[right:]
-                should_merge = should_merge or should_merge_line(before_selector, after_selector, ps[i - 1].line)
-                line.line = before_selector + after_selector
+        prev_line = ps[i - 1].line if i != 0 else ""
+        should_merge = modify_selectors(index_to_sel, line, prev_line)
+        should_merge |= modify_class(class_indices_to_remove, line, prev_line)
 
         if should_merge:
             lines_marked_for_removal.append(line)
@@ -119,6 +160,57 @@ def modify_text(cfunc: cfunc_t, index_to_sel: dict[int, str]):
     # Remove lines that are marked for removal
     for line_to_remove in reversed(lines_marked_for_removal):
         ps.erase(line_to_remove)
+
+
+def modify_selectors(index_to_sel: dict[int, str], line: simpleline_t, prev_line: str):
+    """Try to remove selectors from a line. Returns whether we should merge the line with the previous line"""
+    should_merge = False
+    # Reverse the results so indices will not change
+    for result in reversed(list(re.finditer(SEL_TOKEN_REGEX, line.line))):
+        result: re.Match
+        index = int(result.group("index"), 16)
+        if index in index_to_sel:
+            # We found a selector token, remove it from the list
+            sel = index_to_sel.pop(index)
+            if sel != result.group("selector"):
+                print("[Error]: selector mismatch. Expected:", sel, "Actual:", result.group("selector"))
+                continue
+
+            # If match contains both a prefix and a postfix, remove only the prefix
+            left, right = result.span()
+            if result.group("prefix") and result.group("postfix"):
+                right = result.start("postfix")
+
+            # Remove the selector, check if we need to merge lines
+            before_selector, after_selector = line.line[:left], line.line[right:]
+            should_merge = should_merge or should_merge_line(before_selector, after_selector, prev_line)
+            line.line = before_selector + after_selector
+    return should_merge
+
+
+def modify_class(class_indices_to_remove: set[int], line: simpleline_t, prev_line: str):
+    """Try to remove class from a line. Returns whether we should merge the line with the previous line"""
+    should_merge = False
+
+    # Reverse the results so indices will not change
+    for result in reversed(list(re.finditer(CLASS_TOKEN_REGEX, line.line))):
+        result: re.Match
+        index = int(result.group("index"), 16)
+        index2 = int(result.group("index2"), 16)
+        if index in class_indices_to_remove:
+            # We found a class token, remove it from the list
+            class_indices_to_remove.remove(index)
+
+            if index2 != index + 1:
+                print("[Error]: class indices mismatch for second object. Expected:", index + 1, "Actual:", index2)
+                continue
+
+            # Remove the class, check if we need to merge lines
+            left, right = result.span()
+            before_class, after_class = line.line[:left], line.line[right:]
+            should_merge = should_merge or should_merge_line(before_class, after_class, prev_line)
+            line.line = before_class + after_class
+    return should_merge
 
 
 def should_merge_line(before_selector: str, after_selector: str, prev_line: str) -> bool:
